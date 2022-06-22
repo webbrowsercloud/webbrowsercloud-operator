99c3f68112c0ba8017307ab0cfad0e44a03ba5e8
d4rkcr0w <me@d4rkcr0w.com>
2022-06-23 00:16:20 +0800

    fix: 修复权限

--- controllers/cluster_controller.go
+++ controllers/cluster_controller.go
@@ -47,6 +47,12 @@ type ClusterReconciler struct {
 //+kubebuilder:rbac:groups=webbrowser.cloud,resources=clusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=webbrowser.cloud,resources=clusters/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
